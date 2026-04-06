42afdfa42b729b1fd2422e2ca81e592d947f86bc
Lucas Armani <lucasarmaniribeiroarmani@gmail.com>
2026-04-06 14:47:28 -0300

    Dashboard Completo

--- ctrl_home.py
+++ ctrl_home.py
@@ -2,6 +2,8 @@
 Esse arquivo é um exemplo de controller
 """
 
+from turtle import title
+
 from flask import Blueprint, request, redirect, url_for, render_template, flash, session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -16,5 +18,12 @@ def index(): # função que gerencia rota
     if 'user' not in session:
         return redirect(url_for("auth.login"))
     
-    return render_template("index.html") # Renderiza um template
+    return render_template("dashboard/index.html") # Renderiza um template
+
+@bp.route("/dashboard") # cria uma rota
+def dashboard(): # função que gerencia rota
+    """ Painel de Vendas"""
+    # if 'user' not in session:
+    #     return redirect(url_for("auth.login"))
     
+    return render_template("dashboard/index.html", title="Painel de Vendas") # Renderiza um template
