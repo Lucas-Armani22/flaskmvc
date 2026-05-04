a88e1656f5d74e4617e41acba10a36f27513120e
Lucas <lucasarmaniribeiroarmani@gmail>
2026-05-04 09:06:51 -0300

    Tentando fazer o dashboard funcionar

--- ctrl_home.py
+++ ctrl_home.py
@@ -2,8 +2,6 @@
 Esse arquivo é um exemplo de controller
 """
 
-from turtle import title
-
 from flask import Blueprint, request, redirect, url_for, render_template, flash, session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -15,15 +13,15 @@ bp = Blueprint(__name__, "HomeController")
 @bp.route("/") # cria uma rota
 def index(): # função que gerencia rota
     """ Página inicial"""
-    if 'user' not in session:
+    if '_user_id' not in session:
         return redirect(url_for("auth.login"))
     
     return render_template("dashboard/index.html") # Renderiza um template
 
-@bp.route("/dashboard") # cria uma rota
-def dashboard(): # função que gerencia rota
+@bp.route("/dashboard") # cria uma rota para navegador
+def dashboard(): # função que gerencia rota deve ser única
     """ Painel de Vendas"""
-    # if 'user' not in session:
-    #     return redirect(url_for("auth.login"))
+    #  if 'user' not in session:  # garante autenticação
+    #       return redirect(url_for("auth.login"))
     
     return render_template("dashboard/index.html", title="Painel de Vendas") # Renderiza um template
